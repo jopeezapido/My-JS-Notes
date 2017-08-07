0c2709a84606dadd77990fc35adb9b80916fb42c
Jopee Zapido <jopeezapido@users.noreply.github.com>
2017-08-07 22:53:09 +0800

    add .html, .js and .txt files

---
+++ JSON and APIs/json-ajax-lwwc_yt.js
@@ -0,0 +1,18 @@
+var animalContainer = document.getElementById("animal-info")
+var btn = document.getElementById("btn");
+
+btn.addEventListener("click", function(){
+	var ourRequest = new XMLHttpRequest();
+	ourRequest.open('GET','https://learnwebcode.github.io/json-example/animals-1.json');
+	ourRequest.onload = function (){
+		var ourData = JSON.parse(ourRequest.responseText);
+		renderHTML(ourData);
+	};
+	ourRequest.send();
+});
+
+function renderHTML(data){
+	var htmlString = "this is a test";
+	animalContainer.insertAdjacentHTML('beforeend','htmlString');	
+}
+
