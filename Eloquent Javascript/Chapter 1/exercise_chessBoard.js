4ce0d458b4b71ff81bc05f80aa6154563dc911b3
Jopee Zapido <jopeezapido@users.noreply.github.com>
2017-08-12 11:26:45 +0800

    modified file

--- Eloquent Javascript/Chapter 1/exercise_chessBoard.js
+++ Eloquent Javascript/Chapter 1/exercise_chessBoard.js
@@ -1,15 +1,19 @@
 
-function oddColumn(num,height,width){
+function oddColumn(num,height){
 	
 	for (i = 0; i < height; i++){
 		num += 1;
 		if (num % 2 == 0){
-			console.log('#')
+			console.log('#',' ','#',' ','#',' ','#')
 		}
 		else if (num % 2 != 0){
-			console.log(' ')
+			console.log(' ','#',' ','#',' ','#',' ','#')
 		}
 	}
 }
 
-oddColumn(0,8,1);
+oddColumn(0,8);
+
+/*function x(num,height){
+
+}*/
